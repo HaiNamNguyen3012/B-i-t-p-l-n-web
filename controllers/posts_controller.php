f0877ed1684d0419ce5053d3c4029f2f3859e89b
Tran Nguyen Quang Huy <huyvip2000ht@gmail.com>
2020-12-12 21:23:57 +0700

    update lại

--- controllers/posts_controller.php
+++ controllers/posts_controller.php
@@ -16,9 +16,9 @@ class PostsController extends BaseController
     session_start();
     //tiến hành kiểm tra là người dùng đã đăng nhập hay chưa
     //nếu chưa, chuyển hướng người dùng ra lại trang đăng nhập
-    if (!isset($_SESSION['username'])) {
+ /*   if (!isset($_SESSION['username'])) {
       header('Location: index.php?controller=login&action=index');
-    }
+    }*/
     $posts = Post::all();
     $data = array('posts' => $posts);
     $this->render('index', $data);
--- models/login.php
+++ models/login.php
@@ -15,11 +15,19 @@ class Login{
     }
     
     static function checkUser(){
+        
         var_dump($_POST);
         if(enoughInfo()){
             $db = DB::getInstance();
            // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $user = new Login();
+
+            if($user->vai_tro = "nguoi_cho_thue"){
+
+            }
+            else if($user->vai_tro = "nguoi_thue_phong"){
+                
+            }
 
             $query="SELECT * , count(*) as tong from $user->vai_tro where ten_tai_khoan='$user->ten_tai_khoan'";
             echo $query;//////////////////////// loi loi
@@ -32,6 +40,7 @@ class Login{
                 
                     if(password_verify($user->mat_khau,$info['mat_khau'])){
                         //lưu vào session
+                        session_start();
                         $_SESSION['username']=$user->ten_tai_khoan;
                         echo "đăng nhập thành công";
                         header('Location: index.php?controller=posts');
